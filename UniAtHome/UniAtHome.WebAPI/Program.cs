916be17893a944f0c6e4926dd04b266c32acb453
EdShel <eduardchelemetjev@gmail.com>
2020-10-18 13:55:30 +0300

    Created separate config file where put db connection string

--- UniAtHome/UniAtHome.WebAPI/Program.cs
+++ UniAtHome/UniAtHome.WebAPI/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace UniAtHome.WebAPI
@@ -12,6 +13,12 @@ namespace UniAtHome.WebAPI
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddJsonFile("confidsettings.json", 
+                        optional: false, 
+                        reloadOnChange: true);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
