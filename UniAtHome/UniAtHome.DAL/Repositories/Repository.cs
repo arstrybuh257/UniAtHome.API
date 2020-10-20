0635562489b70753281241ac0f29a02f643c6f73
Valeriia Slostina <valeriia.slostina@nixsolutions.com>
2020-10-20 08:50:32 +0300

    add generic repo

---
+++ UniAtHome/UniAtHome.DAL/Repositories/Repository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using UniAtHome.DAL.Entities;
+using UniAtHome.DAL.Interfaces;
+
+namespace UniAtHome.DAL.Repositories
+{
+    public class Repository : IRepository<BaseEntity> 
+    {
+        protected readonly DbContext context;
+
+        public Repository(DbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<BaseEntity> GetByIdAsync(int id)
+        {
+            return await context.Set<BaseEntity>().FindAsync(id);
+        }
+
+        public async Task AddAsync(BaseEntity item)
+        {
+            await context.Set<BaseEntity>().AddAsync(item);
+        }
+    }
+}
---
+++ UniAtHome/UniAtHome.WebAPI/Controllers/CourseController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UniAtHome.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> GetAllCourses()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("{id}", Name = "GetCourseById")]
+        public string GetCourseById(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public void CreateCourse([FromBody] string value)
+        {
+        }
+
+        [HttpPut("{id}")]
+        public void UpdateCourse(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
