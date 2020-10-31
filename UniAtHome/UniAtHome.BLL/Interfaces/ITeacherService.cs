d951c78663d141b2d92dd30347378f5f8920f32d
Valeriia Slostina <valeriia.slostina@nixsolutions.com>
2020-10-31 10:49:39 +0200

    methods for registering teacher and getting their courses

---
+++ UniAtHome/UniAtHome.BLL/Interfaces/ITeacherService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using UniAtHome.BLL.DTOs;
+using UniAtHome.BLL.DTOs.Auth;
+
+namespace UniAtHome.BLL.Interfaces
+{
+    public interface ITeacherService
+    {
+        Task<IEnumerable<CourseDTO>> GetTeahersCoursesAsync(string teacherEmail);
+
+        Task<RegistrationResponse> RegisterTeacherAsync(RegistrationRequest request);
+    }
+}
--- UniAtHome/UniAtHome.BLL/Services/TeacherService.cs
+++ UniAtHome/UniAtHome.BLL/Services/TeacherService.cs
@@ -1,10 +1,59 @@
-﻿using System;
+﻿using AutoMapper;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using UniAtHome.BLL.DTOs;
+using UniAtHome.BLL.DTOs.Auth;
+using UniAtHome.BLL.Interfaces;
+using UniAtHome.DAL.Entities;
+using UniAtHome.DAL.Interfaces;
+using UniAtHome.DAL.Repositories;
 
 namespace UniAtHome.BLL.Services
 {
-    class TeacherService
+    public class TeacherService: AuthService
     {
+        private readonly IRepository<Teacher> teacherRepository;
+
+        private readonly IRepository<Course> courseRepository;
+
+        private readonly IMapper mapper;
+
+        public TeacherService(
+            UsersRepository usersRepository,
+            IAuthTokenGenerator tokenGenerator,
+            IRefreshTokenFactory refreshTokenFactory,
+            IRepository<Teacher> teacherRepository,
+            IRepository<Course> courseRepository,
+            IMapper mapper) : base(usersRepository, tokenGenerator, refreshTokenFactory)
+        {
+            this.teacherRepository = teacherRepository;
+            this.courseRepository = courseRepository;
+            this.mapper = mapper;
+        }
+
+        public async Task<IEnumerable<CourseDTO>> GetTeahersCoursesAsync(string teacherEmail)
+        {
+            var courses = await courseRepository.Find(
+                course => course.CourseMembers.Any(
+                    members => members.Teacher.User.Email == teacherEmail));
+
+            return this.mapper.Map<IEnumerable<CourseDTO>>(courses);
+        }
+
+        public async Task<RegistrationResponse> RegisterTeacherAsync(RegistrationRequest request)
+        {
+            //KOSTIL`
+            var regiserResult = await this.RegisterAsync(request);
+
+            if (!regiserResult.IdentityResult.Succeeded)
+            {
+                return new RegistrationResponse(regiserResult.IdentityResult.Errors);
+            }
+
+            await this.teacherRepository.AddAsync(new Teacher { UserId = regiserResult.UserId });
+            await this.teacherRepository.SaveChangesAsync();
+            return new RegistrationResponse();
+        }
     }
 }
