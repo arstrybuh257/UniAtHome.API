1c647e8f8cbc789e90d42634d615a1b8d48156b7
Valeriia Slostina <valeriia.slostina@nixsolutions.com>
2020-10-31 18:58:26 +0200

    add lessons service

---
+++ UniAtHome/UniAtHome.BLL/Interfaces/ILessonService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using UniAtHome.BLL.DTOs;
+
+namespace UniAtHome.BLL.Interfaces
+{
+    public interface ILessonService
+    {
+        Task<LessonDTO> GetLessonByIdAsync(int id);
+
+        Task AddLessonAsync(LessonDTO course);
+
+        Task<IEnumerable<LessonDTO>> GetLessonsByCourseIdAsync(string name);
+
+        //temporary returning boolean
+        Task<bool> DeleteLessonAsync(int id);
+    }
+}
---
+++ UniAtHome/UniAtHome.BLL/Services/LessonService.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UniAtHome.BLL.DTOs;
+using UniAtHome.BLL.Interfaces;
+using UniAtHome.DAL.Entities;
+using UniAtHome.DAL.Interfaces;
+
+namespace UniAtHome.BLL.Services
+{
+    public class LessonService: ILessonService
+    {
+        private readonly IRepository<Lesson> lessonRepository;
+
+        private readonly IMapper mapper;
+
+        public LessonService(IRepository<Lesson> lessonRepository, IMapper mapper)
+        {
+            this.lessonRepository = lessonRepository;
+            this.mapper = mapper;
+        }
+
+        public async Task<LessonDTO> GetLessonByIdAsync(int id)
+        {
+            return await this.lessonRepository.GetByIdAsync(id);
+        }
+
+        public Task AddLessonAsync(LessonDTO course)
+        {
+
+        }
+
+        public Task<IEnumerable<LessonDTO>> GetLessonsByCourseIdAsync(string name)
+        {
+
+        }
+
+        //temporary returning boolean
+        public Task<bool> DeleteLessonAsync(int id)
+        {
+
+        }
+    }
+}
