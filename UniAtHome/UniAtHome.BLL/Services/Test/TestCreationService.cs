06408b5c50081c6fdf1a4237b75e26ea1e408960
EdShel <eduardchelemetjev@gmail.com>
2020-11-30 18:02:18 +0200

    Added empty test creation service

---
+++ UniAtHome/UniAtHome.BLL/Services/Test/TestCreationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UniAtHome.BLL.DTOs.Test;
+using UniAtHome.BLL.Interfaces.Test;
+using UniAtHome.DAL.Interfaces;
+using TestEntity = UniAtHome.DAL.Entities.Tests.Test;
+
+namespace UniAtHome.BLL.Services.Test
+{
+    public class TestCreationService : ITestCreationService
+    {
+        private readonly IRepository<TestEntity> tests;
+
+    }
+}
