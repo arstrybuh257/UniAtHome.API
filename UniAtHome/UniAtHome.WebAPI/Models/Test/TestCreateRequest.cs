2b268a0a265c433e0e9dfd5d715a8ffe943258c4
EdShel <eduardchelemetjev@gmail.com>
2020-11-30 20:15:46 +0200

    Controller endpoints

---
+++ UniAtHome/UniAtHome.WebAPI/Models/Test/TestCreateRequest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UniAtHome.BLL.DTOs.Test;
+
+namespace UniAtHome.WebAPI.Models.Test
+{
+    public class TestCreateRequest : TestCreateDTO
+    {
+    }
+
+    public class TestQuestionCreateRequest : TestQuestionCreateDTO
+    {
+
+    }
+
+
+    public class TestAnswerCreateRequest : TestAnswerVariantCreateDTO
+    {
+    }
+
+
+    public class TestScheduleCreateRequest : TestScheduleCreateDTO
+    {
+    }
+
+    public class TestEditRequest : TestEditDTO
+    {
+    }
+
+    public class TestQuestionEditRequest : TestQuestionEditDTO
+    {
+
+    }
+
+
+    public class TestAnswerEditRequest : TestAnswerVariantEditDTO
+    {
+    }
+
+
+    public class TestScheduleEditRequest : TestScheduleEditDTO
+    {
+    }
+
+    public class TestDeleteRequest
+    {
+        public int Id { get; set; }
+    }
+
+    public class TestQuestionDeleteRequest
+    {
+        public int Id { get; set; }
+    }
+
+    public class TestAnswerDeleteRequest 
+    {
+        public int Id { get; set; }
+    }
+
+
+    public class TestScheduleDeleteRequest
+    {
+        public int Id { get; set; }
+    }
+}
