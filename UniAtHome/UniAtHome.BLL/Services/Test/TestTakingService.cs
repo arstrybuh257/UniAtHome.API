e8a0841ad70adcc4363043571896415072912182
EdShel <eduardchelemetjev@gmail.com>
2020-12-02 19:27:46 +0200

    Test answer submit service

--- UniAtHome/UniAtHome.BLL/Services/Test/TestTakingService.cs
+++ UniAtHome/UniAtHome.BLL/Services/Test/TestTakingService.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using UniAtHome.BLL.DTOs.Test;
 using UniAtHome.BLL.Exceptions;
@@ -18,6 +17,10 @@ namespace UniAtHome.BLL.Services.Test
     public class TestTakingService : ITestTakingService
     {
         private readonly IRepository<TestEntity> tests;
+
+        private readonly IRepository<TestAnswerVariant> answerVariants;
+
+        private readonly IRepository<TestAnsweredQuestion> answeredQuestions;
 
         private readonly IStudentService studentService;
 
@@ -77,18 +80,62 @@ namespace UniAtHome.BLL.Services.Test
 
             var generatedTest = await testGenerator.GenerateTestAsync(test);
             generatedTest.AttemptId = testAttempt.Id;
+            return generatedTest;
+        }
 
+        public async Task SubmitAnswerAsync(AnswerSubmitDTO submit)
+        {
+            var student = await students.GetSingleOrDefaultAsync(s => s.User.Email == submit.Email);
+            var attempt = await attempts.GetByIdAsync(submit.AttempId);
+            if (attempt == null)
+            {
+                throw new BadRequestException("Not valid attempt!");
+            }
+            if (attempt.UserId == student.UserId)
+            {
+                throw new ForbiddenException("Isn't the attempt of the user!");
+            }
+            if (attempt.EndTime != null)
+            {
+                throw new ForbiddenException("Can't send answers after the attempt is over!");
+            }
+            var test = await tests.GetByIdAsync(attempt.TestId);
+            if (attempt.BeginTime.AddMinutes(test.DurationMinutes) >= DateTimeOffset.UtcNow)
+            {
+                await FinishAsync(attempt.Id, submit.Email);
+                throw new ForbiddenException("The time is over!");
+            }
+
+            var correctAnswers = await answerVariants.Find(
+                a => a.QuestionId == submit.QuestionId && a.IsCorrect);
+            var correctAnswersIds = correctAnswers.Select(a => a.Id);
+            bool allVaraintsAreCorrect = correctAnswers.Count() == submit.SelectedAnswers.Count()
+                && submit.SelectedAnswers.Intersect(correctAnswersIds).Count() == submit.SelectedAnswers.Count();
+
+            var previousAnswer = await answeredQuestions.GetSingleOrDefaultAsync(
+                aq => aq.AttempId == submit.AttempId && aq.QuestionId == submit.QuestionId);
+            if (previousAnswer != null)
+            {
+                previousAnswer.IsCorrect = allVaraintsAreCorrect;
+                answeredQuestions.Update(previousAnswer);
+            }
+            else
+            {
+                var questionAswer = new TestAnsweredQuestion
+                {
+                    AttempId = submit.AttempId,
+                    QuestionId = submit.QuestionId,
+                    IsCorrect = allVaraintsAreCorrect
+                };
+                await answeredQuestions.AddAsync(questionAswer);
+            }
+            await answeredQuestions.SaveChangesAsync();
         }
 
         public async Task<TestFinishedDTO> FinishAsync(int attemptId, string email)
         {
             throw new NotImplementedException();
         }
-        public async Task SubmitAnswerAsync(AnswerSubmitDTO submit)
-        {
-            throw new NotImplementedException();
-        }
-
         public async Task<IEnumerable<TestFinishedDTO>> GetAllAttemptsAsync(int testId, string email)
         {
             throw new NotImplementedException();
